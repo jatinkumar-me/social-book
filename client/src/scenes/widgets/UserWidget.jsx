23940de08c32586b4a639b3b9fb8d3046acc1614
jatinKumar <jatin208015@gmail.com>
2023-02-06 13:38:01 +0530

    Fixing the image source bug

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -122,7 +122,7 @@ const UserWidget = ({userId, picturePath}) => {
             </Typography>
             <FlexBetween gap="1rem" mb="0.5rem">
                <FlexBetween gap="1rem">
-                  <img src="./src/assets/twitter.png" alt="twitter" />
+                  <img src="/assets/twitter.png" alt="twitter" />
                   <Box>
                      <Typography color={main} fontWeight="500">
                         Twitter
@@ -134,7 +134,7 @@ const UserWidget = ({userId, picturePath}) => {
             </FlexBetween>
             <FlexBetween gap="1rem">
                <FlexBetween gap="1rem">
-                  <img src="./src/assets/linkedin.png" alt="linkedin" />
+                  <img src="/assets/linkedin.png" alt="linkedin" />
                   <Box>
                      <Typography color={main} fontWeight="500">
                         Linkedin
